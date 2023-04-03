f4809fc5806fec5f95d3c100d9e37a9a7a254439
Guillaume Kulakowski <guillaume@kulakowski.fr>
2023-04-03 16:25:52 +0200

    Add BBCode in theme

--- extend.php
+++ extend.php
@@ -17,9 +17,10 @@ return [
         // Add view folder
         ->namespace('theme-fedorafr', __DIR__ . '/views'),
 
-    // Replace code by Emoji
+    // Replace plugins in theme code for modifications
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
+            // Replace Emoji plugin
             $config->Emoticons->add(':)', '🙂');
             $config->Emoticons->add(':-)', '🙂');
             $config->Emoticons->add(':D', '😃');
@@ -33,5 +34,21 @@ return [
             $config->Emoticons->add(':\'(', '😢');
             $config->Emoticons->add(':O', '😮');
             $config->Emoticons->add('>:(', '😡');
+            // Replace BBCode plugin
+            $config->BBCodes->addFromRepository('B');
+            $config->BBCodes->addFromRepository('I');
+            $config->BBCodes->addFromRepository('U');
+            $config->BBCodes->addFromRepository('S');
+            $config->BBCodes->addFromRepository('URL');
+            $config->BBCodes->addFromRepository('IMG');
+            $config->BBCodes->addFromRepository('EMAIL');
+            $config->BBCodes->addFromRepository('CODE');
+            $config->BBCodes->addFromRepository('QUOTE');
+            $config->BBCodes->addFromRepository('LIST');
+            $config->BBCodes->addFromRepository('DEL');
+            $config->BBCodes->addFromRepository('COLOR');
+            $config->BBCodes->addFromRepository('CENTER');
+            $config->BBCodes->addFromRepository('SIZE');
+            $config->BBCodes->addFromRepository('*');
         })
 ];
