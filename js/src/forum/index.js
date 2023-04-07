74374b1a9170938f1b5aeb1d428db68222969287
Guillaume Kulakowski <guillaume@kulakowski.fr>
2023-04-07 11:39:42 +0200

    [Issue #10] Prevent full refresh

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,18 +1,36 @@
-import {extend, override} from 'flarum/common/extend';
+import { extend, override } from "flarum/common/extend";
 
 // We provide our extension code in the form of an "initializer".
 // This is a callback that will run after the core has booted.
-app.initializers.add('fedora-fr-theme', function(app) {
-  var btn = $('#backToTop');
-  $(window).scroll(function() {
+app.initializers.add("fedora-fr-theme", function (app) {
+  // Back to the top button.s
+  var ffrBackToTop = $("#backToTop");
+  $(window).scroll(function () {
     if ($(window).scrollTop() > 300) {
-      btn.addClass('show');
+      ffrBackToTop.addClass("show");
     } else {
-      btn.removeClass('show');
+      ffrBackToTop.removeClass("show");
     }
   });
-  btn.on('click', function(e) {
+  ffrBackToTop.on("click", function (e) {
     e.preventDefault();
-    $('html, body').animate({scrollTop:0}, '300');
+    $("html, body").animate({ scrollTop: 0 }, "300");
   });
+
+  // Prevent full refresh
+  var ffrHomeLink = $(".ffr-home-link");
+  for (var i = 0; i < ffrHomeLink.length; i++) {
+    ffrHomeLink[i].addEventListener("click", (e) => {
+      if (e.ctrlKey || e.metaKey || e.button === 1) return;
+      e.preventDefault();
+      app.history.home();
+
+      // Reload the current user so that their unread notification count is refreshed.
+      const userId = app.session.user?.id();
+      if (userId) {
+        app.store.find("users", userId);
+        m.redraw();
+      }
+    });
+  }
 });
